6e1cba59b4ee48a3d571c971d312d8d46db9a16b
Sergey Kravchuk <s.kravchuk@robo.financ>
2018-09-27 13:03:27 +0700

    Create some seed

---
+++ DML.sql
@@ -0,0 +1,77 @@
+# Скрипты должны выполняться в уже созданной БД
+# Скрипт заполнения БД, выполняется 1 раз
+# Перед заполнением БД, таблицы должны быть созданы !
+
+# Внесение данных о игроках и
+# их регистрации
+
+drop procedure IF EXISTS doiterate;
+create procedure doiterate(IN p1 int)
+  BEGIN
+    label1: LOOP
+      IF p1 > 0
+      THEN
+        #         данные о игроках
+        INSERT INTO player (level, nickname, rating, created, modified)
+        VALUES (p1, CONCAT('test', p1), p1, NOW(), NOW());
+        #         данные о их регистрации
+        INSERT INTO player_autorisation
+        (id_player, full_name, email, login, password, created)
+        VALUES ((SELECT id_player
+                 FROM player
+                 WHERE player.nickname = CONCAT('test', p1)),
+                CONCAT('Такой то такойтович', p1), CONCAT('trueEmail', p1, '@verno.com'),
+                CONCAT('testLogin', p1), CONCAT('testPassword', p1), NOW());
+        SET p1 = p1 - 1;
+        ITERATE label1;
+      END IF;
+      LEAVE label1;
+    END LOOP label1;
+  END;
+call doiterate(10);
+drop procedure doiterate;
+
+# Создание типов персонажей
+
+INSERT INTO homestead.character_type (name, discription, base_param1, base_param2, base_param3)
+VALUES ('Warrior', 'true warrior', 10, 20, 30);
+INSERT INTO homestead.character_type (name, discription, base_param1, base_param2, base_param3)
+VALUES ('Mage', 'true mage', 30, 15, 15);
+INSERT INTO homestead.character_type (name, discription, base_param1, base_param2, base_param3)
+VALUES ('Rogue', 'true rogue', 15, 30, 15);
+
+# Создание персонажей для игроков
+
+create procedure doiterate(IN p1 int)
+  BEGIN
+    SET @TypeNum = 1;
+    label1: LOOP
+      IF p1 > 0
+      THEN
+        SET @TypeNum = (@TypeNum) % 3 + 1;
+        INSERT INTO homestead.`character` (id_player, id_character_type, param1,
+                                           param2, param3, created, modified)
+        VALUES ((SELECT id_player
+                 FROM player
+                 WHERE player.nickname = CONCAT('test', p1)),
+                @TypeNum, 25 + @TypeNum, 25 + @TypeNum * @TypeNum, 27, NOW(), NOW());
+        SET @TypeNum = (@TypeNum) % 3 + 1;
+        INSERT INTO homestead.`character` (id_player, id_character_type, param1,
+                                           param2, param3, created, modified)
+        VALUES ((SELECT id_player
+                 FROM player
+                 WHERE player.nickname = CONCAT('test', p1)),
+                @TypeNum, 25 + @TypeNum, 29, 25 + @TypeNum * @TypeNum, NOW(), NOW());
+        SET p1 = p1 - 1;
+        ITERATE label1;
+      END IF;
+      LEAVE label1;
+    END LOOP label1;
+  END;
+call doiterate(10);
+drop procedure doiterate;
+
+# Создание истории персонажей для игроков путем обновления персонажей
+
+
+
